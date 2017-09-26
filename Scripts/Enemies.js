9764a977403868441f0531810d9523bdc3285e1f
Dorian Rosen <dorianrrosen@gmail.com>
2017-09-26 11:23:11 -0600

    update

--- Scripts/Enemies.js
+++ Scripts/Enemies.js
@@ -71,21 +71,13 @@ function move(enemy)
         enemy.body.velocity.y = -550;
     }
 	else if(enemy.world.x < 5){
-<<<<<<< HEAD
 			enemy.direction = 1;
 	}
 	else if(enemy.world.x > 1165){
-=======
-			console.log("change direction");
 			enemy.animations.play('right');
 			enemy.direction = 1;
 	}
-	else if(enemy.world.x > 1165){
-			console.log("change direction");
-			enemy.animations.play('right');
->>>>>>> master
-			enemy.direction = -1;
-	}
+
    else if(player.body.touching.down && Math.abs(player.world.y - enemy.world.y) < 50)
 	{
 		if(enemy.x > player.x){
